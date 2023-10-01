a311c4e8069d03de4b6ca018de6a75b2d58cb714
lempiji <lempiji@gmail.com>
2023-10-01 14:53:26 +0900

    Add JsonSchema construction functions

--- source/openai/common.d
+++ source/openai/common.d
@@ -41,6 +41,16 @@ struct JsonSchema
     }
 
     ///
+    static JsonValue object_(string description, JsonValue[string] properties)
+    {
+        return JsonValue([
+            "type": JsonValue("object"),
+            "description": JsonValue(description),
+            "properties": JsonValue(properties),
+        ]);
+    }
+
+    ///
     static JsonValue object_(JsonValue[string] properties)
     {
         return JsonValue([
@@ -67,12 +77,14 @@ struct JsonSchema
     }
 
     ///
-    static JsonValue string_(string description, string[] enum_)
+    static JsonValue string_(string description, string pattern, ulong minLength, ulong maxLength)
     {
         return JsonValue([
             "type": JsonValue("string"),
             "description": JsonValue(description),
-            "enum": JsonValue(enum_),
+            "pattern": JsonValue(pattern),
+            "minLength": JsonValue(minLength),
+            "maxLength": JsonValue(maxLength),
         ]);
     }
 
@@ -82,18 +94,6 @@ struct JsonSchema
         return JsonValue([
             "type": JsonValue("string"),
             "description": JsonValue(description),
-            "minLength": JsonValue(minLength),
-            "maxLength": JsonValue(maxLength),
-        ]);
-    }
-
-    ///
-    static JsonValue string_(string description, string pattern, ulong minLength, ulong maxLength)
-    {
-        return JsonValue([
-            "type": JsonValue("string"),
-            "description": JsonValue(description),
-            "pattern": JsonValue(pattern),
             "minLength": JsonValue(minLength),
             "maxLength": JsonValue(maxLength),
         ]);
@@ -110,11 +110,48 @@ struct JsonSchema
     }
 
     ///
+    static JsonValue string_(string description, string[] enum_)
+    {
+        return JsonValue([
+            "type": JsonValue("string"),
+            "description": JsonValue(description),
+            "enum": JsonValue(enum_),
+        ]);
+    }
+
+    ///
+    static JsonValue string_(string description)
+    {
+        return JsonValue([
+            "type": JsonValue("string"),
+            "description": JsonValue(description),
+        ]);
+    }
+
+    ///
+    static JsonValue string_()
+    {
+        return JsonValue([
+            "type": JsonValue("string"),
+        ]);
+    }
+
+    ///
     static JsonValue integer_(string description, long minimum, long maximum)
     {
         return JsonValue([
             "type": JsonValue("integer"),
             "description": JsonValue(description),
+            "minimum": JsonValue(minimum),
+            "maximum": JsonValue(maximum),
+        ]);
+    }
+
+    ///
+    static JsonValue integer_(long minimum, long maximum)
+    {
+        return JsonValue([
+            "type": JsonValue("integer"),
             "minimum": JsonValue(minimum),
             "maximum": JsonValue(maximum),
         ]);
@@ -149,6 +186,16 @@ struct JsonSchema
     }
 
     ///
+    static JsonValue number_(double minimum, double maximum)
+    {
+        return JsonValue([
+            "type": JsonValue("number"),
+            "minimum": JsonValue(minimum),
+            "maximum": JsonValue(maximum),
+        ]);
+    }
+
+    ///
     static JsonValue number_(string description)
     {
         return JsonValue([
@@ -176,4 +223,134 @@ struct JsonSchema
             "maxItems": JsonValue(maxItems),
         ]);
     }
+
+    ///
+    static JsonValue array_(JsonValue items, ulong minItems, ulong maxItems)
+    {
+        return JsonValue([
+            "type": JsonValue("array"),
+            "items": JsonValue(items),
+            "minItems": JsonValue(minItems),
+            "maxItems": JsonValue(maxItems),
+        ]);
+    }
+
+    ///
+    static JsonValue array_(string description, JsonValue items)
+    {
+        return JsonValue([
+            "type": JsonValue("array"),
+            "description": JsonValue(description),
+            "items": JsonValue(items),
+        ]);
+    }
+
+    ///
+    static JsonValue array_(JsonValue items)
+    {
+        return JsonValue([
+            "type": JsonValue("array"),
+            "items": JsonValue(items),
+        ]);
+    }
+
+    ///
+    static JsonValue array_(string description)
+    {
+        return JsonValue([
+            "type": JsonValue("array"),
+            "description": JsonValue(description),
+        ]);
+    }
+
+    ///
+    static JsonValue array_()
+    {
+        return JsonValue([
+            "type": JsonValue("array"),
+        ]);
+    }
+}
+
+@("simple weather schema")
+unittest
+{
+    /*
+    {
+        "type": "object",
+        "properties": {
+            "location": {
+                "type": "string",
+                "description": "The city and state, e.g. San Francisco, CA",
+            },
+            "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
+        },
+        "required": ["location"],
+    },
+    */
+    auto weatherSchema = JsonSchema.object_([
+        "location": JsonSchema.string_("The city and state, e.g. San Francisco, CA"),
+        "unit": JsonSchema.string_("string", ["celsius", "fahrenheit"]),
+    ], ["location"]);
+}
+
+@("nested weather schema")
+unittest
+{
+    /*
+    {
+        "name": "get_current_weather",
+        "description": "Get the current weather",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "location": {
+                    "type": "string",
+                    "description": "The city and state, e.g. San Francisco, CA",
+                },
+                "format": {
+                    "type": "string",
+                    "enum": ["celsius", "fahrenheit"],
+                    "description": "The temperature unit to use. Infer this from the users location.",
+                },
+            },
+            "required": ["location", "format"],
+        },
+    },
+    */
+    auto get_current_weather_params = JsonSchema.object_([
+        "location": JsonSchema.string_("The city and state, e.g. San Francisco, CA"),
+        "format": JsonSchema.string_("The temperature unit to use. Infer this from the users location.", ["celcius", "farenheit"])
+    ], ["location", "format"]);
+
+    /*
+    {
+        "name": "get_n_day_weather_forecast",
+        "description": "Get an N-day weather forecast",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "location": {
+                    "type": "string",
+                    "description": "The city and state, e.g. San Francisco, CA",
+                },
+                "format": {
+                    "type": "string",
+                    "enum": ["celsius", "fahrenheit"],
+                    "description": "The temperature unit to use. Infer this from the users location.",
+                },
+                "num_days": {
+                    "type": "integer",
+                    "description": "The number of days to forecast",
+                }
+            },
+            "required": ["location", "format", "num_days"]
+        },
+    },
+    */
+    auto get_n_day_weather_forecast_params = JsonSchema.object_([
+        "location": JsonSchema.string_("The city and state, e.g. San Francisco, CA"),
+        "format": JsonSchema.string_("The temperature unit to use. Infer this from the users location.", ["celcius", "farenheit"]),
+        "num_days": JsonSchema.integer_("The number of days to forecast"),
+    ], ["location", "format", "num_days"]);
 }
