97e5c0f3f3380a05a9fa3e9828b5822ce82a179d
lempiji <lempiji@gmail.com>
2024-02-03 21:41:38 +0900

    Add support embedding dimensions

--- examples/embedding/source/app.d
+++ examples/embedding/source/app.d
@@ -14,11 +14,17 @@ void main()
 	float[] embedding = response.data[0].embedding;
 	writeln(embedding.length); // text-embedding-ada-002 -> 1536
 
-	auto request2 = embeddingRequest(openai.AdaEmbeddingV2, "D is a general-purpose programming language with static typing, systems-level access, and C-like syntax. With the D Programming Language, write fast, read fast, and run fast.");
+	auto request2 = embeddingRequest(openai.TextEmbedding3Large, "D is a general-purpose programming language with static typing, systems-level access, and C-like syntax. With the D Programming Language, write fast, read fast, and run fast.", 512);
 	auto response2 = client.embedding(request2);
 	float[] embedding2 = response2.data[0].embedding;
+	writeln(embedding2.length); // 512
+
+	auto request3 = embeddingRequest(openai.TextEmbedding3Small, "D is a general-purpose programming language with static typing, systems-level access, and C-like syntax. With the D Programming Language, write fast, read fast, and run fast.", 512);
+	auto response3 = client.embedding(request3);
+	float[] embedding3 = response3.data[0].embedding;
+	writeln(embedding3.length); // 512
 
 	import std.numeric : cosineSimilarity;
-	const similarity = cosineSimilarity(embedding, embedding2);
+	const similarity = cosineSimilarity(embedding2, embedding3);
 	writeln(similarity);
 }
--- source/openai/embedding.d
+++ source/openai/embedding.d
@@ -12,7 +12,6 @@ import openai.common;
 
 @safe:
 
-
 ///
 alias EmbeddingInput = Algebraic!(string, string[]);
 
@@ -20,11 +19,19 @@ alias EmbeddingInput = Algebraic!(string, string[]);
 struct EmbeddingRequest
 {
     ///
+    EmbeddingInput input;
+
+    ///
     @serdeIgnoreDefault
     string model;
 
     ///
-    EmbeddingInput input;
+    @serdeIgnoreDefault
+    @serdeKeys("encoding_format")
+    string encodingFormat;
+
+    @serdeIgnoreDefault
+    uint dimensions;
 
     ///
     @serdeIgnoreDefault
@@ -41,6 +48,42 @@ EmbeddingRequest embeddingRequest(string model, string input)
 }
 
 ///
+unittest
+{
+    auto request = embeddingRequest("text-embedding-ada-002", "Hello, D Programming Language!");
+
+    import mir.ser.json : serializeJson;
+
+    string requestJson = request.serializeJson();
+
+    assert(
+        requestJson == `{"input":"Hello, D Programming Language!","model":"text-embedding-ada-002"}`);
+}
+
+///
+EmbeddingRequest embeddingRequest(string model, string input, uint dimensions)
+{
+    auto request = EmbeddingRequest();
+    request.model = model;
+    request.input = input;
+    request.dimensions = dimensions;
+    return request;
+}
+
+///
+unittest
+{
+    auto request = embeddingRequest("text-embedding-3-small", "Hello, D Programming Language!", 512);
+
+    import mir.ser.json : serializeJson;
+
+    string requestJson = request.serializeJson();
+
+    assert(
+        requestJson == `{"input":"Hello, D Programming Language!","model":"text-embedding-3-small","dimensions":512}`);
+}
+
+///
 EmbeddingRequest embeddingRequest(string model, string[] inputs)
 {
     auto request = EmbeddingRequest();
@@ -48,6 +91,45 @@ EmbeddingRequest embeddingRequest(string model, string[] inputs)
     request.input = inputs;
     return request;
 }
+
+///
+unittest
+{
+    auto inputs = ["Hello,", "D", "Programming", "Language!"];
+    auto request3 = embeddingRequest("text-embedding-ada-003", inputs);
+
+    import mir.ser.json : serializeJson;
+
+    string requestJson3 = request3.serializeJson();
+
+    assert(
+        requestJson3 == `{"input":["Hello,","D","Programming","Language!"],"model":"text-embedding-ada-003"}`);
+}
+
+///
+EmbeddingRequest embeddingRequest(string model, string[] inputs, uint dimensions)
+{
+    auto request = EmbeddingRequest();
+    request.model = model;
+    request.input = inputs;
+    request.dimensions = dimensions;
+    return request;
+}
+
+///
+unittest
+{
+    auto inputs = ["Hello,", "D", "Programming", "Language!"];
+    auto request4 = embeddingRequest("text-embedding-3-small", inputs, 256);
+
+    import mir.ser.json : serializeJson;
+
+    string requestJson = request4.serializeJson();
+
+    assert(
+        requestJson == `{"input":["Hello,","D","Programming","Language!"],"model":"text-embedding-3-small","dimensions":256}`);
+}
+
 
 ///
 struct EmbeddingUsage
@@ -62,6 +144,7 @@ struct EmbeddingUsage
 }
 
 ///
+@serdeIgnoreUnexpectedKeys
 struct EmbeddingResponse
 {
     static struct Embedding
