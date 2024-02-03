65984ff55e56d39f35ba9ac8c2d8d0f8269ebff6
lempiji <lempiji@gmail.com>
2024-02-03 20:26:55 +0900

    Remove unavailable models

--- source/openai/models.d
+++ source/openai/models.d
@@ -44,101 +44,17 @@ enum GPT3Dot5Turbo0301 = "gpt-3.5-turbo-0301";
 ///
 enum GPT3Dot5Turbo = "gpt-3.5-turbo";
 ///
-deprecated("'text-davinci-003' is to be removed by 2024-01-04. Please use 'gpt-3.5-turbo-instruct' instead")
-enum TextDavinci003 = "text-davinci-003";
-///
-deprecated("'text-davinci-002' is to be removed by 2024-01-04. Please use 'gpt-3.5-turbo-instruct' instead")
-enum TextDavinci002 = "text-davinci-002";
-///
-deprecated("'text-curie-001' is to be removed by 2024-01-04. Please use 'gpt-3.5-turbo-instruct' instead")
-enum TextCurie001 = "text-curie-001";
-///
-deprecated("'text-babbage-001' is to be removed by 2024-01-04. Please use 'gpt-3.5-turbo-instruct' instead")
-enum TextBabbage001 = "text-babbage-001";
-///
-deprecated("'text-ada-001' is to be removed by 2024-01-04. Please use 'gpt-3.5-turbo-instruct' instead")
-enum TextAda001 = "text-ada-001";
-///
-deprecated("'text-ada-001' is to be removed by 2024-01-04. Please use 'gpt-3.5-turbo-instruct' instead")
-enum TextDavinci001 = "text-davinci-001";
-///
 enum DavinciInstructBeta = "davinci-instruct-beta";
 ///
-deprecated("'davinci' is to be removed by 2024-01-04. Please use 'gpt-3.5-turbo-instruct' instead")
-enum Davinci = "davinci";
-///
 enum CurieInstructBeta = "curie-instruct-beta";
-///
-deprecated("'curie' is to be removed by 2024-01-04. Please use 'gpt-3.5-turbo-instruct' instead")
-enum Curie = "curie";
-///
-deprecated("'ada' is to be removed by 2024-01-04. Please use 'gpt-3.5-turbo-instruct' instead")
-enum Ada = "ada";
-///
-deprecated("'babbage' is to be removed by 2024-01-04. Please use 'gpt-3.5-turbo-instruct' instead")
-enum Babbage = "babbage";
 
 // Embedding
 ///
 enum AdaEmbeddingV2 = "text-embedding-ada-002";
 ///
-deprecated("'text-similarity-ada-001' is to be removed by 2024-01-04. Please use 'text-embedding-ada-002' instead")
-enum AdaSimilarity = "text-similarity-ada-001";
-///
-deprecated("'text-similarity-ada-001' is to be removed by 2024-01-04. Please use 'text-embedding-ada-002' instead")
-enum BabbageSimilarity = "text-similarity-babbage-001";
-///
-deprecated("'text-similarity-ada-001' is to be removed by 2024-01-04. Please use 'text-embedding-ada-002' instead")
-enum CurieSimilarity = "text-similarity-curie-001";
-///
-deprecated("'text-similarity-ada-001' is to be removed by 2024-01-04. Please use 'text-embedding-ada-002' instead")
-enum DavinciSimilarity = "text-similarity-davinci-001";
-///
-deprecated("'text-similarity-ada-001' is to be removed by 2024-01-04. Please use 'text-embedding-ada-002' instead")
-enum AdaSearchDocument = "text-search-ada-doc-001";
-///
-deprecated("'text-similarity-ada-001' is to be removed by 2024-01-04. Please use 'text-embedding-ada-002' instead")
-enum AdaSearchQuery = "text-search-ada-query-001";
-///
-deprecated("'text-similarity-ada-001' is to be removed by 2024-01-04. Please use 'text-embedding-ada-002' instead")
-enum BabbageSearchDocument = "text-search-babbage-doc-001";
-///
-deprecated("'text-similarity-ada-001' is to be removed by 2024-01-04. Please use 'text-embedding-ada-002' instead")
-enum BabbageSearchQuery = "text-search-babbage-query-001";
-///
-deprecated("'text-similarity-ada-001' is to be removed by 2024-01-04. Please use 'text-embedding-ada-002' instead")
-enum CurieSearchDocument = "text-search-curie-doc-001";
-///
-deprecated("'text-similarity-ada-001' is to be removed by 2024-01-04. Please use 'text-embedding-ada-002' instead")
-enum CurieSearchQuery = "text-search-curie-query-001";
-///
-deprecated("'text-similarity-ada-001' is to be removed by 2024-01-04. Please use 'text-embedding-ada-002' instead")
-enum DavinciSearchDocument = "text-search-davinci-doc-001";
-///
-deprecated("'text-similarity-ada-001' is to be removed by 2024-01-04. Please use 'text-embedding-ada-002' instead")
-enum DavinciSearchQuery = "text-search-davinci-query-001";
-///
 enum AdaCodeSearchCode = "ada-code-search-code";
 ///
-deprecated("'text-similarity-ada-001' is to be removed by 2024-01-04. Please use 'text-embedding-ada-002' instead")
-enum AdaCodeSearchCode001 = "code-search-ada-code-001";
-///
 enum AdaCodeSearchText = "ada-code-search-text";
-///
-deprecated("'text-similarity-ada-001' is to be removed by 2024-01-04. Please use 'text-embedding-ada-002' instead")
-enum AdaCodeSearchText001 = "code-search-ada-text-001";
-///
-deprecated("'text-similarity-ada-001' is to be removed by 2024-01-04. Please use 'text-embedding-ada-002' instead")
-enum BabbageCodeSearchCode = "code-search-babbage-code-001";
-///
-deprecated("'text-similarity-ada-001' is to be removed by 2024-01-04. Please use 'text-embedding-ada-002' instead")
-enum BabbageCodeSearchCode001 = "code-search-babbage-code-001";
-///
-deprecated("'text-similarity-ada-001' is to be removed by 2024-01-04. Please use 'text-embedding-ada-002' instead")
-enum BabbageCodeSearchText = "code-search-babbage-text-001";
-///
-deprecated("'text-similarity-ada-001' is to be removed by 2024-01-04. Please use 'text-embedding-ada-002' instead")
-enum BabbageCodeSearchText001 = "code-search-babbage-text-001";
 
 // Moderation
 ///
