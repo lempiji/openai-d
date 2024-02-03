28baeee43bc30d826c72e716c98ece7a35fcb127
lempiji <lempiji@gmail.com>
2024-02-03 20:50:21 +0900

    Update models

--- source/openai/models.d
+++ source/openai/models.d
@@ -17,6 +17,14 @@ import mir.serde;
 
 // Chat & Completion
 ///
+enum GPT40125Preview = "gpt-4-0125-preview";
+///
+enum GPT4TurboPreview = "gpt-4-turbo-preview";
+///
+enum GPT41106Preview = "gpt-4-1106-preview";
+///
+enum GPT4VisionPreview = "gpt-4-vision-preview";
+///
 enum GPT432K0613 = "gpt-4-32k-0613";
 ///
 deprecated("'gpt-4-32k-0314' is to be removed by 2024-06-13. Please use 'gpt-4-32k-0613' instead")
@@ -31,12 +39,18 @@ enum GPT40314 = "gpt-4-0314";
 ///
 enum GPT4 = "gpt-4";
 ///
+enum GPT350Turbo0125 = "gpt-3.5-turbo-0125";
+///
+enum GPT350Turbo1106 = "gpt-3.5-turbo-1106";
+///
 enum GPT3Dot5TurboInstruct = "gpt-3.5-turbo-instruct";
 ///
+deprecated("'gpt-3.5-turbo-16k-0613' is to be removed by 2024-06-13. Please use 'gpt-3.5-turbo-1106' instead")
 enum GPT3Dot5Turbo16K0613 = "gpt-3.5-turbo-16k-0613";
 /// 
 enum GPT3Dot5Turbo16K = "gpt-3.5-turbo-16k";
 ///
+deprecated("'gpt-3.5-turbo-0613' is to be removed by 2024-06-13. Please use 'gpt-3.5-turbo-1106' instead")
 enum GPT3Dot5Turbo0613 = "gpt-3.5-turbo-0613";
 ///
 deprecated("'gpt-3-turbo-0314' is to be removed by 2024-06-13. Please use 'gpt-3-turbo-0613' instead")
@@ -50,11 +64,11 @@ enum CurieInstructBeta = "curie-instruct-beta";
 
 // Embedding
 ///
+enum TextEmbedding3Large = "text-embedding-3-large";
+///
+enum TextEmbedding3Small = "text-embedding-3-small";
+///
 enum AdaEmbeddingV2 = "text-embedding-ada-002";
-///
-enum AdaCodeSearchCode = "ada-code-search-code";
-///
-enum AdaCodeSearchText = "ada-code-search-text";
 
 // Moderation
 ///
@@ -62,7 +76,13 @@ enum ModerationTextStable = "text-moderation-stable";
 ///
 enum ModerationTextLatest = "text-moderation-latest";
 ///
-enum ModerationText001 = "text-moderation-001";
+enum ModerationText007 = "text-moderation-007";
+
+// GPT base
+///
+enum Babbage002 = "babbage-002";
+///
+enum Davinci002 = "davinci-002";
 
 /// Describes an OpenAI model offering that can be used with the API.
 @serdeIgnoreUnexpectedKeys
